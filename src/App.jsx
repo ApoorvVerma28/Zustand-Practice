bce288d5d32a5678ff4071af25161d6393e3adad
Apoorv Verma <143904933+ApoorvVerma28@users.noreply.github.com>
2026-01-03 23:17:58 +0530

    Add Zustand state management comments to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+// Import React library
 import React from 'react'
+// Import Counter component demonstrating basic Zustand usage
 import Counter from './Components/Counter'
+// Import Cart component showing Zustand for shopping cart state
 import Cart from './Components/Cart'
 
+// Main App component showcasing Zustand state management examples
 const App = () => {
   return (
     <div className= ''>
+     {/* Counter component - currently commented out */}
      {/* <Counter/> */}
+     {/* Cart component - demonstrates Zustand store for cart management */}
      <Cart/>
     </div>
   )
 }
 
+// Export App as default
 export default App
